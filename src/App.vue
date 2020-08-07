a06b0bc59b6dbabbdba21e163734589b3c5daab1
MagallonJanjan <magallonjanjan@gmail.com>
2020-08-07 09:37:42 +0800

    updated 080720

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <v-main>
+      <Header/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Header from '@/components/Header';
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
